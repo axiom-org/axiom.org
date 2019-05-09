df064149d7ae486769ed4dffb08b050f8ba3ff39
Kevin Lacker <lacker@gmail.com>
2019-05-09 14:05:58 -0700

    get docs css in while removing conflict

---
+++ src/pages/docs.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "../styles/animate.css";
+import "../styles/bootstrap.css";
+import "../styles/dark.css";
+import "../styles/docs.css";
+import "../styles/font-icons.css";
+import "../styles/magnific-popup.css";
+import "../styles/responsive.css";
+import "../styles/style.css";
+
+export default () => <div>XXX insert docs here</div>;
